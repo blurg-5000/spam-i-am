d158faa1f8d61a32199eddd66f63b78741f3f2d0
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-07-19 16:58:37 +1200

    prop drill selected cell and image

--- client/Pages/WhackASpam.tsx
+++ client/Pages/WhackASpam.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react'
 import Table from '../components/Table/Table'
+import getRandomNumber from '../utils/getRandomNumber'
 
 function WhackASpam() {
+  const [seletedCell, setSelectedCell] = useState<null | number>(null)
+
+  useEffect(() => {
+    let randomCell = getRandomNumber(0, 24) as number
+    console.log(randomCell)
+    setSelectedCell(randomCell)
+  }, [getRandomNumber])
+
   return (
     <div className="flex items-center justify-center">
-      <Table data={5} />
+      <Table
+        data={5}
+        selectedCell={seletedCell}
+        image={'./images/14-classic2.png'}
+      />
     </div>
   )
 }
