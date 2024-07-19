7667bb6cb0ed9f420c5aebdcaefa964e65a6c0a7
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-07-19 16:17:20 +1200

    add component for new game

---
+++ client/Pages/WhackASpam.tsx
@@ -0,0 +1,11 @@
+import Table from '../components/Table/Table'
+
+function WhackASpam() {
+  return (
+    <div className="flex items-center justify-center">
+      <Table data={5} />
+    </div>
+  )
+}
+
+export default WhackASpam
