3a674b5f876e5a97a0dc5125ae080dbcaa6a539a
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-07-23 11:29:03 +1200

    whack that spam

--- client/Pages/WhackASpam.tsx
+++ client/Pages/WhackASpam.tsx
@@ -1,22 +1,33 @@
 import { useEffect, useState } from 'react'
 import Table from '../components/Table/Table'
 import getRandomNumber from '../utils/getRandomNumber'
+import playAudio from '../utils/playAudio'
 
 function WhackASpam() {
   const [seletedCell, setSelectedCell] = useState<null | number>(null)
 
   useEffect(() => {
+    chooseRandomCell()
+  }, [])
+
+  function chooseRandomCell() {
     let randomCell = getRandomNumber(0, 24) as number
     console.log(randomCell)
     setSelectedCell(randomCell)
-  }, [getRandomNumber])
+  }
+
+  function whackThatSpam() {
+    playAudio('./sounds/squish_sound.mp3')
+    chooseRandomCell()
+  }
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex flex-col items-center justify-center">
+      <h1>WHACK A SPAM</h1>
       <Table
-        data={5}
         selectedCell={seletedCell}
         image={'./images/14-classic2.png'}
+        whackThatSpam={whackThatSpam}
       />
     </div>
   )
