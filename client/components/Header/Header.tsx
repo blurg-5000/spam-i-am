8025568a363a5e726e4ff09873504f5622786bc0
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-07-23 14:13:04 +1200

    refactor

--- client/components/Header/Header.tsx
+++ client/components/Header/Header.tsx
@@ -27,21 +27,3 @@ function Header() {
   )
 }
 export default Header
-
-// <header>
-// <h1>React Kata</h1>
-// <nav>
-//   <p>Choose a kata to display</p>
-//   <button>Fun with Numbers</button>
-//   <button>Pixel Party</button>
-//   <button>List Shenanigans</button>
-//   <button>Neglect the Pumpkins</button>
-// </nav>
-// </header>
-// <main>
-// {activeKata === 'select' && <Select />}
-// {activeKata === 'funWithNumbers' && <FunWithNumbers />}
-// {activeKata === 'pixelParty' && <PixelParty />}
-// {activeKata === 'listShenanigans' && <ListShenanigans />}
-// {activeKata === 'neglectThePumpkins' && <NeglectThePumpkins />}
-// </main>
