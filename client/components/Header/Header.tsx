d583862ff5845c6379d9564619d5a17f8edc9582
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-07-23 18:35:26 +1200

    add link to logo

--- client/components/Header/Header.tsx
+++ client/components/Header/Header.tsx
@@ -11,11 +11,13 @@ function Header() {
   return (
     <header className="bg-spamYellow px-3">
       <nav className="flex items-center justify-between">
-        <img
-          src="./images/14-classic2.png"
-          alt="a beautiful classic can of spam"
-          className="hover:animate-rotate360 duration-400 w-24 transform p-3 transition"
-        />
+        <NavLink to="/">
+          <img
+            src="./images/14-classic2.png"
+            alt="a beautiful classic can of spam"
+            className="hover:animate-rotate360 duration-400 w-24 transform p-3 transition"
+          />
+        </NavLink>
         <ul className="flex items-center space-x-3">
           {menuItems.map((item) => (
             <li className="text-spamBlue font-heading font-heading-bold text-heading-sm p-3">
