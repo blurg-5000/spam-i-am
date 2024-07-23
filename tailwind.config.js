77ffde57692afc4020d119d39014743cf9537270
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-07-23 14:12:14 +1200

    add custom colors and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './client/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        spamYellow: '#facc15',
+        spamBlue: '#1e3a8a',
+      },
+      keyframes: {
+        rotate360: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      animation: {
+        rotate360: 'rotate360 1s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
