1d34099441d7aa4b4b5d976b2970a13b662483b1
Daphne Simons <daphne.simons@devacademy.co.nz>
2024-07-24 11:57:54 +1200

    create db functions for spam and ratings

--- server/db/spam.ts
+++ server/db/spam.ts
@@ -4,3 +4,27 @@ import { Spam } from '../../models/spam.ts'
 export async function getAllSpams(db = connection): Promise<Spam[]> {
   return db('spam').select()
 }
+
+export async function getSpam(id: number, db = connection): Promise<Spam> {
+  return db('spam').where({ id }).first()
+}
+
+export async function createSpam(spam: Spam, db = connection) {
+  return db('spam').insert(spam)
+}
+
+export async function deleteSpam(id: number, db = connection) {
+  return db('spam').where({ id }).del()
+} 
+
+export async function updateSpam(id: number, spam: Spam, db = connection) {
+  return db('spam').where({ id }).update(spam)
+}     
+
+export async function getRating(spamId: number, db = connection) {
+  return db('ratings').where('id', spamId).avg('rating as average_rating').groupBy('spam_id')
+}
+
+export async function addRating(spamId: number, rating: number, userId: number, db = connection) {
+  return db('ratings').insert({ spam_id: spamId, rating, user_id: userId }).onConflict().merge(['rating'])
+} 
