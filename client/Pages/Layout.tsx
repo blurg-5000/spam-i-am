f355fece532b1d05c3d0af9340b6a36da97b72b2
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-07-23 14:12:32 +1200

    render header

--- client/Pages/Layout.tsx
+++ client/Pages/Layout.tsx
@@ -1,11 +1,10 @@
 import { Outlet } from 'react-router-dom'
+import Header from '../components/Header/Header'
 
 export default function Layout() {
   return (
     <>
-      <header>
-        <h1>Fullstack Boilerplate - with Spams!</h1>
-      </header>
+      <Header />
       <main>
         <Outlet />
       </main>
