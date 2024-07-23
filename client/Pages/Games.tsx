07a7a4ad73e938c01351e5bfde9fdd4c24cefdbb
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-07-23 18:35:53 +1200

    render a game

--- client/Pages/Games.tsx
+++ client/Pages/Games.tsx
@@ -1,6 +1,8 @@
+import WhackASpam from './WhackASpam'
+
 function Games() {
   // TODO: Implement pixel party style nav here which statefully shows each game
-  return <p>this is the games page</p>
+  return <WhackASpam />
 }
 
 export default Games
