4f64fc398687523ea7c5758a49c708a310976c7d
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-07-23 17:35:29 +1200

    add footer component

--- client/Pages/Layout.tsx
+++ client/Pages/Layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
 
 export default function Layout() {
   return (
@@ -8,7 +9,7 @@ export default function Layout() {
       <main>
         <Outlet />
       </main>
-      <footer></footer>
+      <Footer />
     </>
   )
 }
