037175e724ee58cf7022c1d1278f9d9a4a381d45
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-07-23 18:38:52 +1200

    add todos

--- client/Pages/Games.tsx
+++ client/Pages/Games.tsx
@@ -2,6 +2,11 @@ import WhackASpam from './WhackASpam'
 
 function Games() {
   // TODO: Implement pixel party style nav here which statefully shows each game
+  // TODO: Add a timer utils for time based games
+  // TODO: Add a score utils for scoring games
+  // TODO: Add a 2D platformer spam game
+  // TODO: Gamify WhackASpam more, by making the spam keep moving around and when you manage to click on it, your score increases
+
   return <WhackASpam />
 }
 
