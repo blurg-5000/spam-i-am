2490d73d31eaf2c3697e7e4e83e7e43a8203603b
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-07-23 15:19:14 +1200

    update links

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,3 +1,12 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
 import App from './App.tsx'
-export default createRoutesFromElements(<Route index element={<App />} />)
+import About from './Pages/About.tsx'
+import Games from './Pages/Games.tsx'
+import Quiz from './Pages/Quiz.tsx'
+export default createRoutesFromElements(
+  <Route path="/" element={<App />}>
+    <Route path="about" element={<About />} />
+    <Route path="games" element={<Games />} />
+    <Route path="quiz" element={<Quiz />} />
+  </Route>,
+)
