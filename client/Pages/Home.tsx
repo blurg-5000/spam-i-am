ad3e8fa4b0157aa02d1ad925679320d76b06f27b
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-07-23 18:36:18 +1200

    add Home component

---
+++ client/Pages/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="flex items-center justify-center">
+      <img src="./images/12-spams.jpeg" className="h-fit" />
+    </div>
+  )
+}
+
+export default Home
