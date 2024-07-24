de757472ef6aead03ad442f12607e136a672ef1d
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-07-25 09:50:35 +1200

    render celebration page with confetti

--- client/Pages/YouWinPage.tsx
+++ client/Pages/YouWinPage.tsx
@@ -1,16 +1,99 @@
+import { useEffect } from 'react'
+import Button from '../components/UI/Button'
+import getRandomNumber from '../utils/getRandomNumber'
+import Confetti from 'react-confetti'
+
 interface Props {
   isReset: boolean
   setIsReset: React.Dispatch<React.SetStateAction<boolean>>
+  canvasRef: React.RefObject<HTMLCanvasElement>
 }
 
-function YouWinPage({ isReset, setIsReset }: Props) {
+function YouWinPage({ isReset, setIsReset, canvasRef }: Props) {
   function resetGame() {
     setIsReset(true)
   }
+
+  const randomNum = getRandomNumber(0, 22)
+  console.log(randomNum)
+
+  const spamImages = [
+    '1-bacon-2',
+    '1-bacon',
+    '2-cheese',
+    '3-hickory-2',
+    '3-hickory',
+    '4-cheese-2',
+    '4-lite-2',
+    '4-lite',
+    '5-black-pepper',
+    '6-hot-n-spicy-2',
+    '6-hot-n-spicy',
+    '7-turkey-2',
+    '7-turkey',
+    '8-jalapeno-2',
+    '8-jalapeno',
+    '9-chorizo',
+    '10-teriyaki',
+    '11-sodium',
+    '12-garlic',
+    '13-port-sausage',
+    '14-classic2',
+    '15-tocino',
+    '16-maple',
+  ]
+
+  function drawSpam(
+    ctx: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    radius: number,
+    color: string,
+  ) {
+    ctx.fillStyle = color
+    ctx.beginPath()
+    ctx.moveTo(x + radius, y)
+    ctx.lineTo(x + width - radius, y)
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius)
+    ctx.lineTo(x + width, y + height - radius)
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height)
+    ctx.lineTo(x + radius, y + height)
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius)
+    ctx.lineTo(x, y + radius)
+    ctx.quadraticCurveTo(x, y, x + radius, y)
+    ctx.closePath()
+    ctx.fill()
+  }
+
+  const colors = ['#facc15', '#1e3a8a', '#f87171']
+
   return (
     <>
-      <p>You won!</p>
-      <button onClick={resetGame}>Play again</button>
+      <section className="flex flex-col items-center justify-center p-5">
+        <Confetti
+          drawShape={(ctx) => {
+            const x = -10 // Adjust these values to center the shape
+            const y = -10 // Adjust these values to center the shape
+            const width = 20
+            const height = 20
+            const radius = 5
+            const color = colors[Math.floor(Math.random() * colors.length)]
+            drawSpam(ctx, x, y, width, height, radius, color)
+          }}
+        />
+        <h1 className="font-heading text-heading-lg font-heading-bold text-spamBlue p-5">
+          Congratulations!
+        </h1>
+        <p className="font-body p-5">
+          You've won a lifetime supply of this spam:{' '}
+        </p>
+        <img src={`./images/${spamImages[randomNum]}.png`} />
+        <Button>
+          <button onClick={resetGame}>Play again</button>
+        </Button>
+      </section>
     </>
   )
 }
