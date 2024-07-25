95d6ede8eec8f8d10c96c4efa57cf97acbf76d5f
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-07-25 14:30:06 +1200

    add menu for games

--- client/Pages/Games.tsx
+++ client/Pages/Games.tsx
@@ -1,14 +1,38 @@
+import { useState } from 'react'
 import SpamJump from './SpamJump'
 import WhackASpam from './WhackASpam'
+import Button from '../components/UI/Button'
+import prettifyCamelCase from '../utils/prettifyCamelCase'
 
 function Games() {
-  // TODO: Implement pixel party style nav here which statefully shows each game
   // TODO: Add a timer utils for time based games
   // TODO: Add a score utils for scoring games
-  // TODO: Add a 2D platformer spam game
   // TODO: Gamify WhackASpam more, by making the spam keep moving around and when you manage to click on it, your score increases
 
-  return <SpamJump />
+  const [activeGame, setActiveGame] = useState('')
+
+  const games = ['spamJump', 'whackASpam']
+
+  return (
+    <>
+      <header>
+        <h1>Choose a game!</h1>
+        <nav>
+          {games.map((game) => (
+            <Button onClick={() => setActiveGame(game)}>
+              <button onClick={() => setActiveGame(game)}>
+                {prettifyCamelCase(game)}
+              </button>
+            </Button>
+          ))}
+        </nav>
+      </header>
+      <main>
+        {activeGame === 'spamJump' && <SpamJump />}
+        {activeGame === 'whackASpam' && <WhackASpam />}
+      </main>
+    </>
+  )
 }
 
 export default Games
