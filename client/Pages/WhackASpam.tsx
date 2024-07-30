77d0495552265d64e863d5e6d1560e1b2afbfde0
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-07-30 16:12:59 +1200

    add a countdown timer and logic to end game

--- client/Pages/WhackASpam.tsx
+++ client/Pages/WhackASpam.tsx
@@ -2,23 +2,35 @@ import { useEffect, useState } from 'react'
 import Table from '../components/Table/Table'
 import getRandomNumber from '../utils/getRandomNumber'
 import playAudio from '../utils/playAudio'
+import useCountdownTimer from '../utils/useCountdownTimer'
+import formatTimer from '../utils/formatTimer'
+import ScoreCard from '../components/UI/ScoreCard'
 
 function WhackASpam() {
-  const [seletedCell, setSelectedCell] = useState<null | number>(null)
+  const [selectedCell, setSelectedCell] = useState<null | number>(null)
   const [score, setScore] = useState<number>(0)
+  const [gameOver, setGameOver] = useState<boolean>(false)
+
+  const timer = useCountdownTimer(120)
 
   useEffect(() => {
-    chooseRandomCell()
+    chooseRandomCell() // Choose a random cell on component mount
 
     const intervalId = setInterval(() => {
-      chooseRandomCell() // Choose a random cell every few seconds
-    }, 1000) // Adjust the interval time (e.g., 2000ms for 2 seconds)
+      chooseRandomCell() // Choose a random cell every second
+    }, 1000)
 
     // Clean up the interval on component unmount
     return () => {
       clearInterval(intervalId)
     }
   }, [])
+
+  useEffect(() => {
+    if (timer === 0) {
+      setGameOver(true)
+    }
+  }, [timer])
 
   function chooseRandomCell() {
     let randomCell = getRandomNumber(0, 24) as number
@@ -34,12 +46,17 @@ function WhackASpam() {
   return (
     <div className="flex flex-col items-center justify-center p-10">
       <h1>WHACK A SPAM</h1>
+      <p>Time left: {formatTimer(timer)}</p>
       <p>Score: {score}</p>
-      <Table
-        selectedCell={seletedCell}
-        image={'./images/14-classic2.png'}
-        whackThatSpam={whackThatSpam}
-      />
+      {gameOver ? (
+        <ScoreCard score={score} />
+      ) : (
+        <Table
+          selectedCell={selectedCell}
+          image={'./images/14-classic2.png'}
+          whackThatSpam={whackThatSpam}
+        />
+      )}
     </div>
   )
 }
