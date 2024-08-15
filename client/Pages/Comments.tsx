e6cfd891c9a2e7b7dbe47de16d74363c5a0b8433
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-08-15 17:27:56 +1200

    create comments section

---
+++ client/Pages/Comments.tsx
@@ -0,0 +1,12 @@
+import CommentDetails from './CommentDetails'
+
+function Comments() {
+  return (
+    <>
+      <p>This is the comments</p>
+      <CommentDetails />
+    </>
+  )
+}
+
+export default Comments
