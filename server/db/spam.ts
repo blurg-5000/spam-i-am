20e1ccaee1d119c5f9cb7f56c5d6c5fd3125fb7b
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-08-15 15:17:23 +1200

    update rating function name

--- server/db/spam.ts
+++ server/db/spam.ts
@@ -1,5 +1,5 @@
 import connection from './connection.ts'
-import { Spam } from '../../models/spam.ts'
+import { Rating, Spam } from '../../models/spam.ts'
 
 export async function getAllSpams(db = connection): Promise<Spam[]> {
   return db('spam').select()
@@ -10,30 +10,42 @@ export async function getSpam(id: number, db = connection): Promise<Spam> {
 }
 
 export async function createSpam(spam: Spam, db = connection) {
-  console.log(spam);
-  
+  console.log(spam)
+
   return db('spam').insert(spam)
 }
 
 export async function deleteSpam(id: number, db = connection) {
   return db('spam').where({ id }).del()
-} 
+}
 
 export async function updateSpam(id: number, spam: Spam, db = connection) {
   return db('spam').where({ id }).update(spam)
-}     
+}
 
-export async function getAllRatings( db = connection) {
+export async function getAllRatings(db = connection): Promise<Rating[]> {
+  console.log('hit the db')
   return db('ratings').select('*')
 }
-export async function getRating(spamId: number, db = connection) {
-  return db('ratings').where('spam_id', spamId).avg('rating as average_rating').groupBy('spam_id')
+
+export async function getAvgRatingBySpamId(spamId: number, db = connection) {
+  return db('ratings')
+    .where('spam_id', spamId)
+    .avg('rating as average_rating')
+    .groupBy('spam_id')
   // return db('ratings').where('id', spamId).avg('rating as average_rating').groupBy('spam_id')
 }
 
-export async function addRating(spamId: number, rating: number, userId: number, db = connection) {
+export async function addRating(
+  spamId: number,
+  rating: number,
+  userId: number,
+  db = connection,
+) {
   // return db('ratings').insert({ 'spam_id': spamId, rating, 'user_id': userId })
-return db('ratings').insert({'user_id': userId, 'spam_id': spamId, 'rating': rating})
-} 
-
-
+  return db('ratings').insert({
+    user_id: userId,
+    spam_id: spamId,
+    rating: rating,
+  })
+}
