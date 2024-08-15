f50788625c0b0ef287e63c9f4c87cefbd45f83a3
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-08-15 17:26:57 +1200

    add route to single spam page

--- client/routes.tsx
+++ client/routes.tsx
@@ -6,12 +6,14 @@ import Quiz from './Pages/Quiz.tsx'
 import RateSpam from './Pages/RateSpam.tsx'
 import Home from './Pages/Home.tsx'
 import ErrorPage from './Pages/ErrorPage.tsx'
+import SpamDetails from './Pages/SpamDetails.tsx'
 export default createRoutesFromElements(
   <Route path="/" element={<App />} errorElement={<ErrorPage />}>
     <Route index element={<Home />} />
     <Route path="about" element={<About />} />
     <Route path="games" element={<Games />} />
     <Route path="quiz" element={<Quiz />} />
+    <Route path="rate-spam/:id" element={<SpamDetails />} />
     <Route path="rate-spam" element={<RateSpam />} />
   </Route>,
 )
