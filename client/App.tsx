707d4db904db9086de295129575d702d98d2a155
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-08-15 17:26:47 +1200

    remove unused code

--- client/App.tsx
+++ client/App.tsx
@@ -1,12 +1,6 @@
 import Layout from './Pages/Layout'
-import WhackASpam from './Pages/WhackASpam'
-import Table from './components/Table/Table'
-import { useSpams } from './hooks/useSpams'
 
 function App() {
-  const { data } = useSpams()
-  console.log(data)
-
   return (
     <>
       <Layout />
