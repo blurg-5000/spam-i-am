3d1095f823ffe9b786e2ff07984907ef09628a50
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-08-15 17:27:11 +1200

    get spam by id and avg rating by id

--- client/apis/apiClient.ts
+++ client/apis/apiClient.ts
@@ -1,10 +1,22 @@
 import request from 'superagent'
-import { SpamData } from '../../models/spam'
+import { AvgRatingDetails, SpamData } from '../../models/spam'
 
 const rootUrl = '/api/v1'
 
 export function getAllSpams(): Promise<SpamData[]> {
-  return request.get(rootUrl + '/spams').then((res) => {
+  return request.get(`${rootUrl}/spams`).then((res) => {
     return res.body.spams as SpamData[]
+  })
+}
+
+export function getSpamById(id: number): Promise<SpamData> {
+  return request.get(`${rootUrl}/spams/${id}`).then((res) => {
+    return res.body.spam as SpamData
+  })
+}
+
+export function getAvgRatingById(id: number) {
+  return request.get(`${rootUrl}/ratings/${id}`).then((res) => {
+    return res.body.rating as AvgRatingDetails
   })
 }
