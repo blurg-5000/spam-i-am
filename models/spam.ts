cf463f70780d7ccf63f68b93c6c3fda4c1cb3cf0
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-08-16 11:49:37 +1200

    add quiz questions ts interface

--- models/spam.ts
+++ models/spam.ts
@@ -31,3 +31,12 @@ export interface QuizAnswers {
   a4: null | string
   a5: null | string
 }
+
+export interface QuizQuestions {
+  id: number
+  question: string
+  option_1: string
+  option_2: string
+  option_3: string
+  option_4: string
+}
