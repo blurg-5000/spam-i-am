a16923b8b4583fab8083a33d3981adebea29b282
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-08-16 09:39:41 +1200

    resolve duplicate rendering issues with error page

--- client/Pages/ErrorPage.tsx
+++ client/Pages/ErrorPage.tsx
@@ -4,14 +4,12 @@ import Header from '../components/Header/Header'
 function ErrorPage() {
   return (
     <>
-      <Header />
       <section className="flex flex-col items-center justify-center p-5">
-        <h1 className="font-heading text-heading-lg font-heading-bold text-spamBlue p-5">
+        <h1 className="p-5 font-heading text-heading-lg font-heading-bold text-spamBlue">
           Whoops! That page can't be found.
         </h1>
         <img src="./images/spam-gif.gif" />
       </section>
-      <Footer />
     </>
   )
 }
