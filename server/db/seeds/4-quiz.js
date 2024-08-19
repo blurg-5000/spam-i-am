c921e409559b2e089037858627a499f8dcdcecc0
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-08-19 16:12:40 +1200

    finish seeding and make results table

--- server/db/seeds/4-quiz.js
+++ server/db/seeds/4-quiz.js
@@ -2,6 +2,7 @@ export async function seed(knex) {
   // Deletes ALL existing entries
   await knex('questions').del()
   await knex('options').del()
+  await knex('results').del()
 
   // Insert questions and get their IDs
   const questionIds = await knex('questions')
@@ -15,11 +16,6 @@ export async function seed(knex) {
     .returning('id')
 
   console.log(questionIds[0].id)
-
-  // mostly a's: spam classic (classic)
-  // mostly b's: spam hot and spicy (fiery)
-  // mostly c's: spam maple (sweet)
-  // mostly d's: spam turkey (alternative)
 
   // Insert options using the question IDs directly
   await knex('options').insert([
@@ -153,6 +149,38 @@ export async function seed(knex) {
       image: 'fire.jpg',
       text: 'A cavern of fire',
       category: 'b',
+    },
+  ])
+
+  // mostly a's: spam classic (classic)
+  // mostly b's: spam hot and spicy (fiery)
+  // mostly c's: spam maple (sweet)
+  // mostly d's: spam turkey (alternative)
+
+  await knex('results').insert([
+    {
+      category: 'a',
+      name: 'SPAM Classic',
+      image: 'spam_classic_text.png',
+      info: `Just like the original SPAM, you're reliable, timeless, and beloved by many. You have a strong sense of tradition and a knack for keeping things simple and straightforward. People know they can count on you, and your steady nature makes you a comforting presence in any situation. You value consistency and aren't afraid to embrace the tried and true.`,
+    },
+    {
+      category: 'b',
+      name: 'SPAM Hot & Spicy',
+      image: 'spam_hot_n_spicy_text.png',
+      info: `Bold, adventurous, and full of zest, you bring energy and excitement wherever you go. Your fiery personality lights up a room, and you’re always ready to try something new. You thrive on challenges and have a passion that inspires those around you. You’re not one to shy away from the spotlight, and your enthusiasm is truly contagious.`,
+    },
+    {
+      category: 'c',
+      name: 'SPAM Maple',
+      image: 'spam_maple_text.png',
+      info: `Sweet, warm, and irresistibly charming, you have a personality that draws people in. You're thoughtful, caring, and always there to brighten someone’s day. Your kindness leaves a lasting impression, and your gentle nature makes you a trusted friend. You believe in the power of small gestures and have a way of making everyone feel special.`,
+    },
+    {
+      category: 'd',
+      name: 'SPAM Turkey',
+      image: 'spam_turkey_text.png',
+      info: ` Unique, creative, and always thinking outside the box, you’re the alternative choice that stands out in a crowd. You’re not afraid to go against the grain and carve your own path. With a fresh perspective and a love for innovation, you bring new ideas to the table and challenge the status quo. Your originality is your strength, and you’re proud to be different.`,
     },
   ])
 }
