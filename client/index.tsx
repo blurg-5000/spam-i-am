2ea218be3572567135cc082bed1492fa94360f2f
Daphne Simons <daphne.simons@devacademy.co.nz>
2024-08-22 16:24:29 +1200

    setup auth0 provider

--- client/index.tsx
+++ client/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
+import { Auth0Provider } from '@auth0/auth0-react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import routes from './routes.tsx'
 
@@ -10,9 +10,24 @@ const queryClient = new QueryClient()
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>,
+    /**
+     * Auth0Provider is a component that has a hook that provides
+     * all authentication operations
+     *
+     * TODO: replace the empty strings below with your own domain, clientId, and audience
+     */
+    <Auth0Provider
+      domain="spam-matai-2024.au.auth0.com"
+      clientId="Wq4NM9ebbHVPGT6KHd3nq5EVeWwWHT5c"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: 'https://spam/api',
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </Auth0Provider>,
   )
 })
