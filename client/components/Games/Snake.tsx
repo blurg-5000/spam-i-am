4f3fc466b19aa973f47a0196cc997886dafcbc37
Jared Pinfold <j.pinfold@gmail.com>
2024-08-22 14:32:56 +1200

    implement obstacles in snake game

--- client/components/Games/Snake.tsx
+++ client/components/Games/Snake.tsx
@@ -6,15 +6,19 @@ import getRandomNumber from '../../utils/getRandomNumber'
 
 function Snake() {
   const gridSize = 10
+  const speed = 400 // TODO make dynamic - radio buttons?
+  const numOfObstacles = 3 // TODO make dynamic - radio buttons?
   const nums = new Array(gridSize).fill(0).map((_, i) => i ) // used to generate the table programatically
 
-  const initialPosition = `${getRandomNumber(0, gridSize - 1)}${getRandomNumber(0, gridSize - 1)}`
+  const tableRef = useRef<HTMLInputElement>(null) // used in auto focusing on the table
+  const initialCoords = useRef(randomCoords(numOfObstacles + 1))
+  const [initialPosition, ...obstacles] = initialCoords.current
+
   const [head, setHead] = useState(initialPosition) // where the snake's head is. Movement in the grid is based on this
   const [snake, setSnake] = useState([initialPosition]) // every segment of the snake
+  const [food, setFood] = useState([] as string[])
   const [direction, setDirection] = useState('none')
   const [gameState, setGameState] = useState('alive')
-  const tableRef = useRef<HTMLInputElement>(null) // used in auto focusing on the table
-
 
   useEffect(() => {
     if (tableRef.current) {
@@ -25,14 +29,14 @@ function Snake() {
       if (head) {
         const next = nextPosition() // nextPosition also checks if the snake will go out of bounds
         const [noggin, ...tail] = snake
-        if (tail.includes(next)) { // check if snake crashes into itself, but can't crash into it's own head
+        if ([...tail, ...obstacles].includes(next)) { // check if snake crashes into obstacle or itself, but can't crash into it's own head
           setGameState('dead')
         } else if (next) { // ignored if no direction is set yet
           setHead(next)
           setSnake([next, ...snake])
         }
       }
-    }, 400)
+    }, speed) //snake speed
 
     function nextPosition(): string | null {
       const headRow = Number(head[0])
@@ -70,7 +74,25 @@ function Snake() {
     return () => {
       clearInterval(intervalId)
     }
-  }, [head, direction, snake])
+  }, [head, direction, snake, obstacles])
+  
+  function randomCoords(num: number, toBeOmitted: string[] = []): string[] {
+    const coords: string[] = []
+    const omit = [...toBeOmitted]
+
+    for (let i=0; i< num; i++) {
+      const currentCoord = createCoord()
+      coords.push(currentCoord)
+      omit.push(currentCoord)
+    }
+  
+    function createCoord (): string {
+      const currentCoord = `${getRandomNumber(0, gridSize - 1)}${getRandomNumber(0, gridSize - 1)}`
+      return omit.includes(currentCoord) ? createCoord() : currentCoord
+    }
+    console.log({coords})
+    return coords
+  }
 
   function handleKeyDown(e: React.KeyboardEvent<HTMLTableElement>): void {
     if (
@@ -105,7 +127,7 @@ function Snake() {
   return (
     <>
       <h1>Snake</h1>
-      <p>{direction}</p>
+      {/* <p>Use Arrow keys to move - Avoid tofu and eat Spam to unlock your home.</p> */}
       {gameState === 'dead' && <h1>You died!</h1>}
       {gameState === 'win' && <h1>You won!</h1>}
       {gameState === 'alive' && (
@@ -121,7 +143,7 @@ function Snake() {
                 return (
                   <td
                     key={coord}
-                    className={`${coord} h-4 w-4 ${snake.includes(coord) ? 'bg-spamPink' : 'bg-spamBlue'}`}
+                    className={`${coord} h-4 w-4 ${snake.includes(coord) ? 'bg-lime-400' : obstacles.includes(coord) ? 'bg-white' :'bg-spamBlue'}`}
                   ></td>
                 )
               })}
@@ -134,3 +156,4 @@ function Snake() {
 }
 
 export default Snake
+
