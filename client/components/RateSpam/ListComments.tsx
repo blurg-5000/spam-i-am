b2c064f6dcceb1cce08bd477268b3eb441f64c91
Daphne Simons <daphne.simons@devacademy.co.nz>
2024-08-28 10:55:47 +1200

    currently lists comments, create on date, and userId

--- client/components/RateSpam/ListComments.tsx
+++ client/components/RateSpam/ListComments.tsx
@@ -1,9 +1,24 @@
+import { useCommentsById } from '../../hooks/useComments'
+
 function ListComments() {
   // TODO: call a custom hook (that uses useQuery) to get all comments for this specific spam.
-
+  const { data } = useCommentsById(2)
   return (
     <>
-      <p>A list of comments for this SPAM flavour</p>
+      {/* <p>A list of comments for this SPAM flavour</p> */}
+      <ul>
+        {data?.map((comment) => (
+          <>
+            <li>{comment.comment_text}</li>
+            <li>
+              {/* Created on: */}
+              {new Date(Number(comment.created_date)).toLocaleString()}
+            </li>
+            {/* By:  */}
+            <li>{comment.user_id}</li>
+          </>
+        ))}
+      </ul>
     </>
   )
 }
