0950ee88b6cca7de3b1f316e2378d7a793cab228
Jared Pinfold <j.pinfold@gmail.com>
2024-08-28 13:34:13 +1200

    implement difficulty sliders for snake game

--- client/components/Games/Snake.tsx
+++ client/components/Games/Snake.tsx
@@ -1,21 +1,72 @@
 import { useState } from 'react'
 import SnakeGameBoard from './SnakeGameBoard'
 import Button from '../UI/Button'
+import { Slider } from '@mui/material'
 
 function Snake() {
-  const gridSize = 10
-  const speed = 200 // TODO make dynamic - radio buttons?
-  const numOfObstacles = 3 // TODO make dynamic - radio buttons?
-
+  const [speed, setSpeed] = useState(150)
+  const [numOfObstacles, setNumOfObstacles] = useState(3)
   const [showComponent, setShowComponent] = useState('difficulty')
+  
+  const gridSize = 10
+  const marks = [
+    {
+      value: 0,
+      label: 'Easy',
+    },
+    {
+      value: 1,
+      label: 'Medium',
+    },
+    {
+      value: 2,
+      label: 'Hard',
+    },
+  ]
+
+
+  function handleSpeedChange(v: number): void {
+    const speeds = [200, 150, 100]
+    setSpeed(speeds[v])
+  }
+
+  function handleObstaclesChange(v: number): void {
+    const obstacles = [0, 3, 5]
+    setNumOfObstacles(obstacles[v])
+  }
 
   if (showComponent === 'difficulty')
     return (
       <>
-        <p>Speed: {speed}</p>
-        <p>Number of Obstacles: {numOfObstacles}</p>
-        <p>Use Arrow keys to move</p>
-        <p>Eat spam and avoid tofu</p>
+        <p>Speed:</p>
+        <Slider
+          onChange={(_, v) => handleSpeedChange(v as number)}
+          valueLabelDisplay="off"
+          step={null}
+          marks={marks}
+          min={0}
+          max={2}
+          defaultValue={1}
+        />
+        <hr/>
+        <br/>
+        <p>Number of Obstacles:</p>
+        <Slider
+          onChange={(_, v) => handleObstaclesChange(v as number)}
+          valueLabelDisplay="off"
+          step={null}
+          marks={marks}
+          min={0}
+          max={2}
+          defaultValue={1}
+        />
+        <hr />
+        <br />
+        <ul>
+          <li>Use Arrow keys to move</li>
+          <li>Eat spam to grow and increase your score </li>
+          <li>Avoid tofu as it will kill you (allergies)</li>
+        </ul>
         <Button onClick={() => setShowComponent('game')}>Play</Button>
       </>
     )
