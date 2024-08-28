b28b9c1f3b02176bd81c23d14deb2fbf81ee2951
Jared Pinfold <j.pinfold@gmail.com>
2024-08-29 09:41:43 +1200

    prep Snake for ticket

--- client/components/Games/Snake.tsx
+++ client/components/Games/Snake.tsx
@@ -1,65 +1,23 @@
 import { useState } from 'react'
 import SnakeGameBoard from './SnakeGameBoard.tsx'
 import Button from '../UI/Button'
-import { Slider } from '@mui/material'
 
 function Snake() {
-  const [speed, setSpeed] = useState(150)
-  const [numOfObstacles, setNumOfObstacles] = useState(3)
+  const speed = 150
+  const numOfObstacles = 3
   const [showComponent, setShowComponent] = useState('difficulty')
   
   const gridSize = 10
-  const marks = [
-    {
-      value: 0,
-      label: 'Easy',
-    },
-    {
-      value: 1,
-      label: 'Medium',
-    },
-    {
-      value: 2,
-      label: 'Hard',
-    },
-  ]
-
-
-  function handleSpeedChange(v: number): void {
-    const speeds = [200, 150, 100]
-    setSpeed(speeds[v])
-  }
-
-  function handleObstaclesChange(v: number): void {
-    const obstacles = [0, 3, 5]
-    setNumOfObstacles(obstacles[v])
-  }
 
   if (showComponent === 'difficulty')
     return (
       <>
         <p>Speed:</p>
-        <Slider
-          onChange={(_, v) => handleSpeedChange(v as number)}
-          valueLabelDisplay="off"
-          step={null}
-          marks={marks}
-          min={0}
-          max={2}
-          defaultValue={1}
-        />
+        {/* TODO: Make a slider go here */}
         <hr/>
         <br/>
         <p>Number of Obstacles:</p>
-        <Slider
-          onChange={(_, v) => handleObstaclesChange(v as number)}
-          valueLabelDisplay="off"
-          step={null}
-          marks={marks}
-          min={0}
-          max={2}
-          defaultValue={1}
-        />
+        {/* TODO: Make a slider go here */}
         <hr />
         <br />
         <ul>
