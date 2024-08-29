bc6d07167a32ff0cd65d71f691a50893c837f444
Sam <171207654+sam-pedersen@users.noreply.github.com>
2024-08-30 10:25:43 +1200

    refactor listcomments compenent so show data

--- client/components/RateSpam/ListComments.tsx
+++ client/components/RateSpam/ListComments.tsx
@@ -1,17 +1,41 @@
-function ListComments() {
+import { useParams } from 'react-router-dom'
+import { useFetchCommentsBySpamId } from '../../hooks/useComments'
+
+interface Props {
+  spamId: number
+}
+
+function ListComments(props: Props) {
   // TODO: call a custom hook (that uses useQuery) to get all comments for this specific spam.
   // TODO: useParams to get the unique spamId
+  const { spamId } = props
 
-  return (
-    <>
-      <h4>Comments</h4>
-      <ul>
-        <li>{/* Comment text: */}</li>
-        <li>{/* Created on: */}</li>
-        <br></br>
-      </ul>
-    </>
+  const { data, isLoading, isError, error } = useFetchCommentsBySpamId(
+    Number(spamId),
   )
+  if (isLoading) {
+    return <p>Loading</p>
+  }
+
+  if (isError) {
+    console.error(error.message)
+    return <p>error</p>
+  }
+
+  if (data)
+    return (
+      <>
+        <h4>Comments</h4>
+        <ul>
+          {data.map((commentSpamObj) => (
+            <li key={commentSpamObj.id}>{commentSpamObj.comment_text}</li>
+          ))}
+
+          <li>{/* Created on: */}</li>
+          <br></br>
+        </ul>
+      </>
+    )
 }
 
 export default ListComments
