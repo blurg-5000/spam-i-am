53560de1260a3c9fee518fb4e53f46fede232195
Alicia <170913210+alicia-tavita@users.noreply.github.com>
2024-08-30 13:29:50 +1200

    heart icon added

--- client/components/RateSpam/RatingAvg.tsx
+++ client/components/RateSpam/RatingAvg.tsx
@@ -1,3 +1,21 @@
+import { useAvgRatingById } from '../../hooks/useRatings'
+import * as React from 'react'
+import { styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import Rating from '@mui/material/Rating'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import Typography from '@mui/material/Typography'
+
+const StyledRating = styled(Rating)({
+  '& .MuiRating-iconFilled': {
+    color: '#ff6d75',
+  },
+  '& .MuiRating-iconHover': {
+    color: '#ff3d47',
+  },
+})
+
 export default function RatingAvg({ spamId }: { spamId: number }) {
   // TODO: Display the Rating:
   // using the spamId parameter, call `useAvgRatingById` from
@@ -10,10 +28,19 @@ export default function RatingAvg({ spamId }: { spamId: number }) {
 
   return (
     <>
-      {/* TODO: Display a Rating: 
-        // use the data from avgRating to display 
-        // the data using a customisable <Rating/> 
-        // from the MUI library */}
+      <Box sx={{ '& > legend': { mt: 2 } }}>
+        <Typography component="legend">Custom icon and color</Typography>
+        <StyledRating
+          name="customized-color"
+          defaultValue={2}
+          getLabelText={(value: number) =>
+            `${value} Heart${value !== 1 ? 's' : ''}`
+          }
+          precision={0.5}
+          icon={<FavoriteIcon fontSize="inherit" />}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+        />
+      </Box>
     </>
   )
 }
